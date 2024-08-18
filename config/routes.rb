80671f0c9a5fa9b9e4e9442259ab3c77b06c403a
jic <ji_camargo@yahoo.com>
2024-08-17 19:22:41 -0500

    chore: Update database.yml and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Definir la raíz de la API, para evitar errores en production
+  root to: proc { [200, {}, ['API is up and running']] }
+
   namespace :api do
     namespace :v1 do
       resources :productos
